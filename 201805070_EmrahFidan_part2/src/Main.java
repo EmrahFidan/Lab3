d61c0cdcddbeb2e9678e47ed18018fce7a9aa758
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-07 16:34:05 +0300

    in case of incorrect value entry

--- 201805070_EmrahFidan_part2/src/Main.java
+++ 201805070_EmrahFidan_part2/src/Main.java
@@ -3,44 +3,51 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// I created arrays
-		String nameArray[] = new String[3]; 
-		int ageArray[] = new int[3];
-		double weightArray[] = new double[3]; 
-		double heightArray[] = new double[3];
-		String interpretArray[] = new String[3];
-		
-		Scanner scan = new Scanner(System.in);
-		
-		for( int i = 0; i < 3; i++) {
-				
-			System.out.println("Enter name, age, weight, height: (as space separated): ");
-			String input = scan.nextLine();
-			
-			interpretArray[i] = input;
-			
-			String [] inputs = input.split(" ");
-			
-			nameArray[i] = inputs[0].concat(" ").concat(inputs[1]);
-			
-			int age = Integer.valueOf(inputs[2]); 
-			ageArray[i] = age;
-			
-			double weight = Double.valueOf(inputs[3]);
-			weightArray[i] = weight;
-			
-			double height = Double.valueOf(inputs[4]);
-			heightArray[i] = height;
-			
-			System.out.println();
-			}
-			// elements added to series
+		try{
 
-			// output part
-			for (int i=0;i < 3; i++) {
-				System.out.println("---ENTER PERSON "+(i+1)+"'S VALUES---");
-				System.out.println("** The BMI result for "+nameArray[i]+" ("+" Age: "+ageArray[i]+" Weight: "+weightArray[i]+" Height: "+heightArray[i]+")"+" is");
-				System.out.println(interpretArray[i]);
+			// I created arrays
+			String nameArray[] = new String[3]; 
+			int ageArray[] = new int[3];
+			double weightArray[] = new double[3]; 
+			double heightArray[] = new double[3];
+			String interpretArray[] = new String[3];
+			
+			Scanner scan = new Scanner(System.in);
+			
+			for( int i = 0; i < 3; i++) {
+					
+				System.out.println("Enter name, age, weight, height: (as space separated): ");
+				String input = scan.nextLine();
+				
+				interpretArray[i] = input;
+				
+				String [] inputs = input.split(" ");
+				
+				nameArray[i] = inputs[0].concat(" ").concat(inputs[1]);
+				
+				int age = Integer.valueOf(inputs[2]); 
+				ageArray[i] = age;
+				
+				double weight = Double.valueOf(inputs[3]);
+				weightArray[i] = weight;
+				
+				double height = Double.valueOf(inputs[4]);
+				heightArray[i] = height;
+				
+				System.out.println();
+				}
+				// elements added to series
+
+				// output part
+				for (int i=0;i < 3; i++) {
+					System.out.println("---ENTER PERSON "+(i+1)+"'S VALUES---");
+					System.out.println("** The BMI result for "+nameArray[i]+" ("+" Age: "+ageArray[i]+" Weight: "+weightArray[i]+" Height: "+heightArray[i]+")"+" is");
+					System.out.println(interpretArray[i]);
+				}
+		}
+			catch (Exception e) {
+				// in case of incorrect value entry
+				System.out.println("invalid input, please restart program");
 			}
 
 	}
