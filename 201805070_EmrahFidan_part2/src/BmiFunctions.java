126c148db5a206887ff425d55217be0273f1f4cf
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-07 16:35:13 +0300

    I created BmiFunctions Class

--- 201805070_EmrahFidan_part2/src/BmiFunctions.java
+++ 201805070_EmrahFidan_part2/src/BmiFunctions.java
@@ -1,7 +1,39 @@
 	
 public class BmiFunctions {
 
-	
+	public static double calculateBMI(double weight, double height) {
+		
+		weight = (0.45359237) * weight;
+		height = (0.0254) * height;
+		
+		double BMI = weight / (height*height); 
+		
+		return BMI;
+    }
+
+    public static String interpretBMI(double BMI) {
+		
+		String result = null;
+		
+		if(BMI < 18.5) {
+			result = "Underweight";
+		}
+		else if(BMI >= 18.5 && BMI < 25.0) {
+			result = "Normal";
+		}
+			
+		else if(BMI >= 25 && BMI < 30.0) {
+			result = "Owerweight";
+		}
+			
+		else if(BMI >= 30.0) {
+			result = "Obese";
+		}
+			
+		
+		return result;
+		
+	}
 		
 }
 
