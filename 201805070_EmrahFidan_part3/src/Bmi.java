f86559fe43d08b779de6060d5b14159a356d0ad4
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-07 16:40:02 +0300

    I created constructors for part3

--- 201805070_EmrahFidan_part3/src/Bmi.java
+++ 201805070_EmrahFidan_part3/src/Bmi.java
@@ -1,6 +1,34 @@
 
 public class Bmi {
+	// constructor 1
+	public Bmi() {
+		
+	}
 	
+    // constructor 2
+	public Bmi(String name,double weight,double height) {
+		
+		_name = name;
+		_weight = weight;
+		_height = height;
+		
+	}
+
+    // constructor 3
+	public Bmi(String name,int age,double weight,double height) {
+		
+		_name = name;
+		_age = age;
+		_weight = weight;
+		_height = height;
+		
+	}
 	
+	// constructor for defaulr value
+    
+	private String _name;
+	private int _age;
+	private double _weight;
+	private double _height;
 	
 }
