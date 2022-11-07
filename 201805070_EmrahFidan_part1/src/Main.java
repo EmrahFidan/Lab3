77992d0d4b73c97abcd1566189b6445ddc8cdab0
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-07 16:28:08 +0300

    I started part2

--- 201805070_EmrahFidan_part1/src/Main.java
+++ 201805070_EmrahFidan_part1/src/Main.java
@@ -9,10 +9,44 @@ public class Main {
 		System.out.println("Enter weight, height: (as space sperated)");
 		String input = scan.nextLine();
 
-
-
+		try {
+			String [] inputs = input.split(" ");
+			String w = inputs[0];
+			String h = inputs[1];
+			
+			double weight = Float.valueOf(w);
+			double height = Float.valueOf(h);
+			
+			weight = (0.45359237) * weight;
+			height = (0.0254) * height;
+			
+			double BMI = weight / (height*height); 
+			
+			System.out.println();
+			System.out.println("BMI is: "+BMI);
+			
+		if(BMI < 18.5) {
+			System.err.println("Underweight");
+		}
+		else if(BMI >= 18.5 && BMI < 25.0) {
+			System.err.println("Normal");
+		}
+			
+		else if(BMI >= 25 && BMI < 30.0) {
+			System.err.println("Qwerweight");
+		}
+			
+		else if(BMI >= 30.0) {
+			System.err.println("Obese");
+		}
 		
 	}
 
+	catch (Exception error) {
+		System.out.println("input error");
 	}
 
+
+	}
+
+}
--- 201805070_EmrahFidan_part2/src/Main.java
+++ 201805070_EmrahFidan_part2/src/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		String nameArray[] = new String[3]; 
+		int ageArray[] = new int[3];
+		double weightArray[] = new double[3]; 
+		double heightArray[] = new double[3];
+		String interpretArray[] = new String[3];
+		
 		
 
 	}
