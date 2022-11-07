50f0c9b49c38eb360ddd4e346d3e1ebe0ab9b41d
Emrahfidan <201805070@stu.adu.edu.tr>
2022-11-07 16:44:25 +0300

    I created BmiFunctions  in the Bmi Class

--- 201805070_EmrahFidan_part3/src/Bmi.java
+++ 201805070_EmrahFidan_part3/src/Bmi.java
@@ -25,10 +25,59 @@ public class Bmi {
 	}
 	
 	// constructor for defaulr value
-    
+
 	private String _name;
 	private int _age;
 	private double _weight;
 	private double _height;
 	
+    public static final double KILOGRAMS_PER_POUND(double weight) {
+		
+		return (0.45359237) *weight;
+		
+	}
+	
+	public static final double METERS_PER_INCH(double height) {
+		
+		return (0.0254) *height;
+		
+	}
+	
+	public static double getBMI(double weight, double height) {
+		
+		double Weight = KILOGRAMS_PER_POUND(weight);
+		double Height = METERS_PER_INCH(height);
+		
+		double Bmı = Weight / (Height*Height);
+		
+		// I use math.round method
+		double BMI = Math.round(Bmı);
+		
+		return BMI;
+		
+	}
+	
+	public static String getStatus(double BMI) {
+		
+		String result = null;
+		
+		if(BMI < 18.5) {
+			result = "Underweight";
+		}
+		else if(BMI >= 18.5 && BMI < 25.0) {
+			result = "Normal";
+		}
+			
+		else if(BMI >= 25 && BMI < 30.0) {
+			result = "Owerweight";
+		}
+			
+		else if(BMI >= 30.0) {
+			result = "Obese";
+		}
+			
+		return result;
+		
+	}
+
 }
